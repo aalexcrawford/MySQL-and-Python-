5790fc4c05c97ea9bd161fab66c06b48c293de40
aalexcrawford <56134884+aalexcrawford@users.noreply.github.com>
2020-11-16 12:25:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ def verifyLogin():
 	conn = connector.connect()
 	c = conn.cursor()
 	data = request.get_json()
-	uName = data['user_name']
-	hashPassword = data['hashed_password']
+	u_name = data['user_name']
+	h_password = data['hashed_password']
+	sql = "SELECT user_id FROM users WHERE user_name=%s AND hashed_password=%s"
+	val = (str(u_name), str(h_password))
 	try:
-		c.execute("SELECT user_id FROM users WHERE user_name=%s AND hashed_password=%s", (str(uName), str(hashPassword)))
-		rows = c.fetchone()
-		return jsonify({"user_id":rows[0]})
+		c.execute(sql, val)
+		u_id = c.fetchone()
+		return jsonify({"user_id":u_id[0]})
 	except:
 		print("No such user found")
 		return jsonify({"user_id":"-1"})
@@ -28,29 +30,34 @@ def createUser():
 	conn = connector.connect()
 	c = conn.cursor()
 	data = request.get_json()
-	uName = data['user_name']
-	hPassword = data['hashed_password']
-	eMail = data['email']
-	val = (uName, hPassword, eMail)
+	u_name = data['user_name']
+	h_password = data['hashed_password']
+	e_mail = data['email']
 	sql = "INSERT INTO users (user_name, hashed_password, email) VALUES (%s, %s, %s)"
+	val = (u_name, h_password, e_mail)
 	try:
 		c.execute(sql, val)
 		conn.commit()
-		return jsonify({"user_id":"Welcome " + uName})
+		return jsonify({"user_name":"Welcome " + uName})
 	except:
 		print("Failed to create user")
-		return jsonify({"user_id":"error creating user account"})
+		return jsonify({"user_name":"error creating user account"})
 
 # The /request is how we are going to differ between different queries i.e. /requestUserData or /requestContactData
 @app.route('/test', methods=['POST'])
-def printFromJSON():
+def addContact():
 	conn = connector.connect()
-	#data = request.get_json() gets the entire JSON
+	c = conn.cursor()
 	data = request.get_json()
-	#userID = data['id'] get the specific variable in the JSON
-	userID = data['id']
-
-	return '''user ID is {}'''.format(userID)
+	u_id = data['user_id']
+	c_id = data['contact_id']
+	sql = "INSERT INTO contacts (contact_id) VALUES(%S) WHERE user_id=%s"
+	val = (c_id, u_id)
+	try:
+		c.execute(sql,val)
+		return jsonify({"message":"contact successfully added"})
+	except:
+		return jsonify({"message":"contact unable to be added"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
